a9bf81ee504a2be4b9fa3fa8f4f02bd0cc06dac0
Mitchell Cooper <cooper879@msn.com>
2011-04-22 23:52:28 +0000

    load bans on module load and on rehash

--- netban.pm
+++ netban.pm
@@ -39,7 +39,7 @@ sub init {
 
     # create the table if it does not exist.
     if (!&create_db) {
-        utils::snotice('could not create netban database.');
+        snotice('could not create netban database.');
         say 'could not create netban database.';
         return
     }
@@ -50,7 +50,7 @@ sub init {
     # load the stored bans
     load_bans();
 
-    # register the commands
+    # register the commands 
     register_command('kline', 'Ban or unban a user by their user@host mask.', \&handle_kline) or return;
     register_command('zline', 'Ban an IP or IP range.', \&handle_zline) or return;
 
@@ -61,7 +61,7 @@ sub init {
 # create the tables
 sub create_db {
     $dbh->do('CREATE TABLE IF NOT EXISTS kline (mask TEXT, setby TEXT, time INT, reason TEXT)') or return;
-    $dbh->do('CREATE TABLE IF NOT EXISTS zline (ip TEXT, setby TEXT, time INT, resaon TEXT)') or return;
+    $dbh->do('CREATE TABLE IF NOT EXISTS zline (ip TEXT, setby TEXT, time INT, reason TEXT)') or return;
     return 1
 }
 
@@ -77,7 +77,25 @@ sub connect_db {
 
 # load the stored klines
 sub load_bans {
-    say 'loading klines'
+
+    # klines
+    my $sth = $dbh->prepare('SELECT * FROM kline');
+    $sth->execute;
+    while (my $ref = $sth->fetchrow_hashref) {
+        $main::kline{delete $ref->{mask}} = $ref
+    }
+
+    my $sth2 = $dbh->prepare('SELECT * FROM zline');
+    $sth2->execute;
+    while (my $ref = $sth2->fetchrow_hashref) {
+        $main::zline{delete $ref->{ip}} = $ref
+    }
+
+    # check each user for a ban
+    kline_check();
+    zline_check();
+
+    return 1
 }
 
 # handle ZLINE command
@@ -88,4 +106,25 @@ sub handle_zline {
 sub handle_kline {
 }
 
+# check all users for a K-Line
+sub kline_check {
+    $_->checkkline foreach values %user::connection;
+    return 1
+}
+
+# check all users for a Z-Line
+sub zline_check {
+    foreach my $user (values %user::connection) {
+        foreach (keys %main::zline) {
+
+            # found a match!
+            $user->quit('Z-Lined: '.$main::zline{$_}{'reason'},
+                undef,
+                'Z-Lined'.((conf qw/main showzline/) ? q(: ).$main::zline{$_}{'reason'} : q..)
+            ) if (hostmatch($user->{'ip'}, $_));
+
+        }
+    }
+    return 1
+}
 1
